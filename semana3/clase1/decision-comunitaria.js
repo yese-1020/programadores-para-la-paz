631da4fbc183d07d8981ded63dcbadca74339f32
yese-1020 <yess1020x@gmail.com>
2026-04-29 21:54:21 -0500

    agrego semana3

---
+++ semana3/clase1/decision-comunitaria.js
@@ -0,0 +1,12 @@
+const propuesta = "esta noticia es real ?"
+let  comparacionredsocial = false
+let diapublicado = true
+
+console.log("revision de la noticia")
+console.log("propuesta:", propuesta)
+
+if (comparacionredsocial === true && diapublicado === true) {
+console.log("la puede compartirse.")
+} else {
+console.log("la noticia no se puede compartir.")
+}
